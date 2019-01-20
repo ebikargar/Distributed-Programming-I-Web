3dc0fdf3694638a4e1734cbf2e6305d5c85e6e5e
Ebrahim Kargar <ebrahim.kargar@gmail.com>
2019-01-20 22:35:25 +0100

    db config

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+require_once('functions.php');
+$server='localhost';
+$dbname="s202999";
+$userdb='s202999';
+$passdb='dlizatio';
+
+$NumOfFixedSrc = 4;
+
+require_once('connection.php');
+
+
+
+?>
